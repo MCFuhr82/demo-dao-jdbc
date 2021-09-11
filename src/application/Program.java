4e0b88e86996b69047dc1a893c784d804ba13d54
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-09-10 21:19:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(1, "Marcelo", "marcelofuhr@me.com", new Date(), 9500.00, obj);
 		System.out.println(obj);
+		System.out.println(seller);
 	}
-
 }
