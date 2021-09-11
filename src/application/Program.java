dbdd1ceb52a0385e70a6165a281284a4a830b21a
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-09-10 22:39:02 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(1, "Marcelo", "marcelofuhr@me.com", new Date(), 9500.00, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(7);
+		
 		System.out.println(seller);
 	}
 }
