db9cdaa02b4c574388829e2437d11938ca1b9b9e
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-09-10 22:00:17 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,6 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(1, "Marcelo", "marcelofuhr@me.com", new Date(), 9500.00, obj);
