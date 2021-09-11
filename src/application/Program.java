1bca7448ce1795833dc18d11844e0915848914ba
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-09-10 22:08:25 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,9 @@
 package application;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +14,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(1, "Marcelo", "marcelofuhr@me.com", new Date(), 9500.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(obj);
 		System.out.println(seller);
 	}
